b8c1ff4dfb4ddf8fce11b7094e5e34d5199df70b
alexgrove <alexander_grove@hotmail.com>
2018-06-24 11:35:30 -0600

    Added navmesh so the enemies now follow you around

---
+++ Dungeon Crawler/Assets/Scripts/NeutralMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NeutralMovement : MonoBehaviour {
+
+	Transform playerPosition;
+	NavMeshAgent nav;
+
+	// Use this for initialization
+	void Start () 
+	{
+		playerPosition = GameObject.FindGameObjectWithTag ("Player").transform;
+		nav = GetComponent<NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		nav.SetDestination (playerPosition.position);
+	}
+}
