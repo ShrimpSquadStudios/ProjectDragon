af84a4c3021f8fc3e27f38b7f0b07e4a665f5902
alexgrove <alexander_grove@hotmail.com>
2018-06-26 19:04:00 -0600

    Added projectile to Trebuchet. Push space to fire.

---
+++ Trebuchet/Assets/Release.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Release : MonoBehaviour {
+
+	HingeJoint releasePin;
+
+	// Use this for initialization
+	void Start () {
+		releasePin = GetComponent<HingeJoint> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) 
+		{
+			releasePin.breakForce = 0;
+		}
+	}
+
+}
