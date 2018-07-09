4dbab2ec358a404d0dc961b4227e3f465903da59
Nick Klose <nick.klose@ualberta.ca>
2018-07-08 22:53:18 -0600

    Adding test UI to Megan's city building project.

---
+++ Megan - City Building Test/Assets/Controllers/UI_MainController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_MainController : MonoBehaviour {
+
+    public WorldController worldcontroller;
+
+    public Text ironCountText;
+    public Text workerCountText;
+    public Text buildingCountText;
+
+    int ironCount = 0;
+    int workerCount = 0;
+    int buildingCount = 0;
+
+
+	void Start () {
+        World world = worldcontroller.world;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        ironCount = worldcontroller.world.GetIronCount();
+        // workerCount = worldcontroller.world.GetWorkerCount();
+        // buildingCount = worldcontroller.world.GetBuildingCount();
+        ironCountText.text = ironCount.ToString();
+        Debug.Log(ironCount);
+	}
+}
